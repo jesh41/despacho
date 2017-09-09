0d6935f4a619228d5dce0fd545bbd8fcbf05f7c4
ernesto <jesh40@hotmail.es>
2017-09-09 13:03:07 -0600

    bug

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,7 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(UsersTableSeeder::class);
+     //   $this->call(UsersTableSeeder::class);
         $this->call(catsucursalseed::class);
         $this->call(CatEstadoseed::class);
         $this->call(catdespacho::class);
