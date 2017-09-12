80ed57f3dc2d1b5e71f7ce9d2304bb47968b3bd9
ernesto <jesh40@hotmail.es>
2017-09-12 14:50:27 -0600

    nuevas tablas

--- config/app.php
+++ config/app.php
@@ -163,6 +163,7 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
         Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
+        Spatie\Permission\PermissionServiceProvider::class,
 
         /*
          * Package Service Providers...
